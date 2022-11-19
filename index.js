30ef3d02510d55dc48d417dbe792462570caa29e
mehnazimu <mehnaz.imu@gmail.com>
2022-11-19 18:22:12 +0600

    last

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 
 
@@ -18,6 +19,24 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 console.log(uri)
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next){
+    
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send('unauthorized access');
+    }
+    const token = authHeader.split(' ')[1];
+    
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+        if(err){
+            return res.status(403).send({message:'forbidden access'})
+
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 async function run() {
     try {
@@ -46,8 +65,14 @@ async function run() {
             res.send(options);
         });
 
-        app.get('/bookings', async(req,res)=>{
+        app.get('/bookings',verifyJWT, async(req,res)=>{
             const email = req.query.email;
+             const decodedEmail = req.decoded.email;
+            
+             if(email !== decodedEmail){
+                return res.status(403).send({massage: 'forbidden access'});
+             }
+            
             const query = {email:email};
             const bookings = await bookingsCollection.find(query).toArray();
             res.send(bookings);
@@ -72,20 +97,66 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/jwt', async(req,res)=>{
+            const email = req.query.email;
+            const query={email:email};
+            const user = await usersCollection.findOne(query);
+            if(user){
+                const token =jwt.sign({email},process.env.ACCESS_TOKEN,{expiresIn: '24hr'})
+                return res.send({accessToken: token});
+            }
+            res.status(403).send({accessToken:''})
+        });
+
+        app.get('/users', async(req,res)=>{
+            const query={};
+            const users=await usersCollection.find(query).toArray();
+            res.send(users);
+        })
+
+
+
         app.post('/users',async(req,res)=>{
             const user =req.body;
             const result= await usersCollection.insertOne(user);
             res.send(result);
+        });
+
+        app.get('/users/admin/:email', async(req,res)=>{
+            const email = req.params.email;
+            const query = {email}
+            const user = await usersCollection.findOne(query);
+            res.send({isAdmin: user?.role === 'admin'});
         })
 
-        /**
-         * API naming convention
-         * app.get('/bookings')
-         * app.get('/bookings/:id')
-         * app.post('/bookings')
-         * app.patch('/bookings/:id')
-         * app.delete('/bookings/:id')
-         */
+        app.put('/users/admin/:id',verifyJWT, async(req,res)=>{
+            const decodedEmail = req.decoded.email;
+            const query = {email: decodedEmail};
+            const user= await usersCollection.findOne(query);
+
+            if(user?.role !== 'admin'){
+                return res.status(403).send({message: 'forbidden access'})
+            }
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id)}
+            const options = {upsert: true};
+            const updatedDoc ={
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc,options)
+            res.send(result)
+        })
+
+        // /**
+        //  * API naming convention
+        //  * app.get('/bookings')
+        //  * app.get('/bookings/:id')
+        //  * app.post('/bookings')
+        //  * app.patch('/bookings/:id')
+        //  * app.delete('/bookings/:id')
+        //  */
 
 
 
